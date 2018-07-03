13ed35bebc95e5faeb646b0073f3496aaac6ff1b
NATASHAVAL <natasha.valentina1998@gmail.com>
2018-07-03 16:33:58 +0700

    POST add card

--- postboard.py
+++ postboard.py
@@ -52,3 +52,32 @@ print (response.text)
 """
 
 #POST add new card
+boards_url = base + 'members/me/boards'
+board_list = requests.get(boards_url, params=param_butuh)
+board_list_array = board_list.json()
+
+for board in board_list_array:
+    print (board['name'])
+
+board_name = input("Name of a board to add the card? ")
+for board in board_list_array:
+    board_id = board['id']
+
+list_url = base + 'boards/' + board_id + '/lists'
+list_list = requests.request("GET", list_url, params=param_butuh)
+list_list_array = list_list.json()
+
+for lists in list_list_array:
+    print (lists['name'])
+
+list_name = input("Name of the list to add the card on? ")
+for lists in list_list_array:
+    if lists['name'] == list_name:
+        list_id = lists['id']
+
+card_name = input("Name of the card you wished to add? ")
+card_url = base + '/cards'
+card_desc = input("Description to the card? ")
+param_butuh.update({'name': card_name, 'desc': card_desc, 'idList':list_id})
+response = requests.request("POST", card_url, params=param_butuh)
+print(response.text)
