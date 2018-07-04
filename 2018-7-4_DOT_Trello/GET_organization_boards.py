1516ccd261049e3c0efec6d11af6f8d7304cbc94
NATASHAVAL <natasha.valentina1998@gmail.com>
2018-07-04 16:23:29 +0700

    choose list and card

--- 2018-7-4_DOT_Trello/GET_organization_boards.py
+++ 2018-7-4_DOT_Trello/GET_organization_boards.py
@@ -37,6 +37,7 @@ for board in organization_list_array:
     if (board['name'] == board_name):
         board_id = board['id']
 
+
 # 2. List yang ada dalam board tertentu
 list_url = base + 'boards/' + board_id + '/lists'
 list_list = requests.get(list_url, params=params_key_and_token)
@@ -46,38 +47,54 @@ print ('\tJumlah list dalam board :' + str(len(list_list_array)))
 for lists in list_list_array:
         print ('\t - ' + lists['name'])
 
+lists_choice = input('\nTampilkan semua isi card dalam list? (y/n) ')
+
+def funcShowCard(list_id_param, list_name_param):
+    card_url = base + 'lists/' + list_id_param + '/cards'
+    params_key_and_token.update({'fields': 'id,name,desc,pos'})
+    card_list = requests.get(card_url, params=params_key_and_token)
+    card_list_array = card_list.json()
+    print ('\t - ' + list_name_param)
+    print ('\t\tJumlah card dalam list : ' + str(len(card_list_array)))
+    
+    for cards in card_list_array:
+        print ('\t\t -- ' + cards['name'])
+
+# 4. Checklist items yang ada di card tertentu
+        check_url = base + 'cards/' + cards['id'] + '/checklists'
+        if "fields" in params_key_and_token:
+            params_key_and_token.pop("fields")
+        check_list = requests.get(check_url, params=params_key_and_token)
+        check_list_array= check_list.json()
+
+        if len(check_list_array) > 0:
+            print ('\t\t\tJumlah checklist setiap card : ' + str(len(check_list_array)))
+
+        for checklist in check_list_array:
+            belum = 0
+            selesai = 0
+            print ('\t\t\t --- Checklist: ' + checklist['name'])
+            for centang in checklist['checkItems']:
+                print ('\t\t\t\t' + centang['name'] + '->' + centang['state'])
+                if centang['state'] == 'complete':
+                    selesai += 1
+                else:
+                    belum += 1
+            print ('\t\t\tSelesai: ' + str(selesai) + ' Belum: ' + str(belum))
+            if (selesai > 0) or (belum > 0):
+                persentaseselesai = selesai / (selesai+belum) * 100
+                print ('\t\t\tPersentase selesai: ' + str(persentaseselesai) + '%')
+
 # 3. Cards yang ada pada list tertentu
-        card_url = base + 'lists/' + lists['id'] + '/cards'
-        params_key_and_token.update({'fields': 'id,name,desc,pos'})
-        card_list = requests.get(card_url, params=params_key_and_token)
-        card_list_array = card_list.json()
-        print ('\t\tJumlah card dalam list : ' + str(len(card_list_array)))
-        
-        for cards in card_list_array:
-            print ('\t\t -- ' + cards['name'])
+if (lists_choice == 'y') or (lists_choice == 'Y'):
+    for lists in list_list_array:
+        funcShowCard(lists['id'], lists['name'])
 
-#. 4. Checklist items yang ada di card tertentu
-            check_url = base + 'cards/' + cards['id'] + '/checklists'
-            if "fields" in params_key_and_token:
-                params_key_and_token.pop("fields")
-            check_list = requests.get(check_url, params=params_key_and_token)
-            check_list_array= check_list.json()
+else:
+    lists_name = input ('\nNama dari LIST yang ingin menampilkan isi cardnya? ')
+    for lists in list_list_array:
+        if (lists_name == lists['name']):
+            lists_id = lists['id']
+    funcShowCard(lists_id, lists_name)
 
-            if len(check_list_array) > 0:
-                print ('\t\t\tJumlah checklist setiap card : ' + str(len(check_list_array)))
 
-            for checklist in check_list_array:
-                belum = 0
-                selesai = 0
-                print ('\t\t\t --- Checklist: ' + checklist['name'])
-                for centang in checklist['checkItems']:
-                    print ('\t\t\t\t' + centang['name'] + '->' + centang['state'])
-                    if centang['state'] == 'complete':
-                        selesai += 1
-                    else:
-                        belum += 1
-                print ('\t\t\tSelesai: ' + str(selesai) + ' Belum: ' + str(belum))
-                if (selesai > 0) or (belum > 0):
-                    persentaseselesai = selesai / (selesai+belum) * 100
-                    print ('\t\t\tPersentase selesai: ' + str(persentaseselesai) + '%')
-                
