e8bb7e27869c1af18debabd409d262bcc780d77c
NATASHAVAL <natasha.valentina1998@gmail.com>
2018-07-05 16:10:03 +0700

    finish fetch data to database under boardname

--- MongoDB/MONGO_TrelloAction.py
+++ MongoDB/MONGO_TrelloAction.py
@@ -2,22 +2,22 @@
 import pprint
 from pymongo import MongoClient
 client = MongoClient()
-db = client['Trello']
 
-# import from Trello API
+db_name = input('Name your database? ')
+db = client[db_name]
+
+# import from Trello API -> limit of 300 reqs per 10s & no > 100 req / 10s interval
 import requests
 import json
 from trello import TrelloApi
 
-#key = input ('Your Trello API key? ')
-#token = input ('Your Trello token? ')
-key = '19e1e8779951830e0d86122f201454c6'
-token = '6fec5904bd2db30d876191d45f68bbec0db6322e36ebaa2ad80c2bcd7948d5f9' 
+key = input ('Your Trello API key? ')
+token = input ('Your Trello token? ')
 params_key_and_token = {'key':key, 'token':token}
 
 base = 'https://api.trello.com/1/'
 # JANGAN LUPA ganti jadi 'ptdotindonesia1'
-organization_name = 'pythonuser'
+organization_name = 'ptdotindonesia1'
 
 def Merge(dict1, dict2):
     res = {**dict1, **dict2}
@@ -36,7 +36,7 @@ if 'fields' in params_key_and_token:
 
 # Tampilkan jumlah, nama board, tanggal aktivitas terakhir, deskripsi board
 for board in organization_list_array:
-    print (board['name'] + ' id: ' + board['id'])
+    print (board['name'])
     '''if 'dateLastActivity' in board:
         print ('Last Activity: ' + str(board['dateLastActivity']))
     print (board['desc'])'''
@@ -50,20 +50,25 @@ board_url = base + 'boards/' + board_id + '/actions'
 params_key_and_token.update({'fields': 'id,type,date,memberCreator,member'})
 response = requests.request("GET", board_url, params=params_key_and_token)
 board_array = response.json()
+
+print ('Your data will be saved under MongoDB collection named the same as board name')
 c_action = db[board_name]
 
-#print (board_array)
 action_iterator = 1
 for action in board_array:
     print ('\n' + str(action_iterator) + '. ' + action['type'] + ' ' + action['date'])
     print (action['memberCreator']['username'] + ' ' + action['memberCreator']['fullName'])
     if 'member' in action:
         print ('another member: ' + action['member']['username'] + ' ' + action['member']['fullName'])
-    action_iterator += 1
-    #harus melakukan assign baru key id ke _id O(N) agar bisa unique value di MongoDB
-    action['_id'] = action.pop('id')
     
+    action_iterator += 1
+    # harus melakukan assign baru key id ke _id O(N) agar bisa unique value di MongoDB
+    action['_id'] = action.pop('id')
+    # pakai setOnInsert, jika belum ada maka diinsert jika sudah ada maka di ...
+    c_log = c_action.update({'_id': action['_id']}, {'$setOnInsert': action}, upsert=True)
+    #print (c_log)
 
+print ('Action Log has been successfully inserted into database')
 # Masukkan action ke MongoDB collection dengan nama collection = board_name
 '''
 It is significantly faster to use find() + limit() because findOne() will always read + return the document if it exists. find() just returns a cursor (or not) and only reads the data if you iterate through the cursor.
@@ -71,7 +76,7 @@ It is significantly faster to use find() + limit() because findOne() will always
 -- https://blog.serverdensity.com/checking-if-a-document-exists-mongodb-slow-findone-vs-find/
 Better to use "COVERED INDEX"
 '''
-action_insert = c_action.insert_many(board_array)
+# action_insert = c_action.insert_many(board_array)
 
 
 '''
@@ -84,3 +89,5 @@ if filter_choice == 'Y' or filter_choice == 'y':
         if ('member' in action) and (action['member']['fullName'] == filter_name or action['member']['username'] == filter_name):
             print ('-- ' + action['type'] + ' ' + action['date'])
 '''
+
+client.close()
