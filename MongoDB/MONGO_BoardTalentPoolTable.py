dc33e02b48d2ff76210be30d99e620feee61d4cf
NATASHAVAL <natasha.valentina1998@gmail.com>
2018-07-12 23:18:14 +0700

    Create HTML table

--- MongoDB/MONGO_BoardTalentPoolTable.py
+++ MongoDB/MONGO_BoardTalentPoolTable.py
@@ -1,5 +1,6 @@
 '''
 MongoDB Python -> GET data from MongoDB database
+print as HTML table tag
 '''
 import pprint
 from itertools import islice
@@ -19,18 +20,57 @@ for lists in coll.find({'type': 'List'}):
     
 #for card in islice(coll.find({"$and": [{'type': 'Card'},{'labels': {"$ne": None}}]}) , 2):
 #    pprint.pprint(card)
-for lists in coll.find({'type': 'List'}):    
-    for label in coll.find({'type': 'Label'}):
-        if (label['name']=='junior level') or (label['name']=='intermediate level'):
-            continue
-            #trello_table[label['idList']].update
-            #ans = coll.find({"$and": [{'type': 'Card'}, {'labels': label}]}).count()
-        ans = coll.find({"$and": [{'type': 'Card'}, {'idList': lists['_id']}, {'labels.name': {"$all": [label['name'], "junior level"]} }]}).count()
-        if ans > 0:
-          print (lists['name'], label['name'], 'junior level', ans)
-        ans = coll.find({"$and": [{'type': 'Card'}, {'idList': lists['_id']}, {'labels.name': {"$all": [label['name'], "intermediate level"]} }]}).count()
-        if ans > 0:
-          print (lists['name'], label['name'], 'intermediate level', ans)
-    
 
-# search job and list (junior / intermediate)
+# print HTML table
+html_table = """<html><table border="1"><tr><th>Job</th><th>Level</th>"""
+#html_table = html_table + "<th>Job</th><th>Level</th>"
+
+for lists in coll.find({'type': 'List'}):
+    html_list = "<th>"+str(lists['name'])+"</th>"
+    html_table = html_table + html_list
+html_table = html_table + "<th>Total Level</th></tr>"
+    
+# Junior, Intermediate Level in 2 loops
+for label in coll.find({'type': 'Label'}):
+    # Junior Level
+    if (label['name']=='junior level') or (label['name']=='intermediate level'):
+            continue
+    html_table = html_table + "<tr><td>" + label['name'] + "</td><td>Junior</td>"
+    for lists in coll.find({'type': 'List'}):
+        ans = coll.find({"$and": [{'type': 'Card'}, {'idList': lists['_id']}, {'labels.name': {"$all": [label['name'], "junior level"]} }]}).count()
+        
+        if ans > 0:
+            html_card = "<td><b>"+str(ans)+"</b></td>"
+            html_table = html_table + html_card
+            print (lists['name'], label['name'], 'junior level', ans)
+        else:
+            html_card = "<td>"+str(ans)+"</td>"
+            html_table = html_table + html_card
+
+    #total_level = coll.find({"$and": [{'type': 'Card'}, {'labels.name': {"$all": [label['name'], "junior level"]} }]}).count()
+    #html_table = "<td>" + str(total_level) + "</td></tr>"
+    
+    # Intermediate Level
+    html_table = html_table + "<tr><td>" + label['name'] + "</td><td>Intermediate</td>"
+    for lists in coll.find({'type': 'List'}):
+        
+        ans = coll.find({"$and": [{'type': 'Card'}, {'idList': lists['_id']}, {'labels.name': {"$all": [label['name'], "intermediate level"]} }]}).count()
+        
+        if ans > 0:
+            html_card = "<td><b>"+str(ans)+"</b></td>"
+            html_table = html_table + html_card
+            print (lists['name'], label['name'], 'intermediate level', ans)
+        else:
+            html_card = "<td>"+str(ans)+"</td>"
+            html_table = html_table + html_card
+        
+    html_table = html_table + "</tr>"
+    #total_level = coll.find({"$and": [{'type': 'Card'}, {'labels.name': {"$all": [label['name'], "intermediate level"]} }]}).count()
+    #html_table = "<td>" + str(total_level) + "</td></tr>"
+    
+html_table = html_table+"</table></html>"
+hs = open ("htmlTrelloTable.html", "w")
+hs.write(html_table)
+
+#print (html_table)
+hs.close()
